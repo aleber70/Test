1995812058f28deee6ed32841ecb4267381a9646
Alexandr <oberezhnyi.bas@outlook.com>
2021-08-17 15:50:39 +0300

    logo

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,11 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    # if we have a html_logo below, this shows /only/ the logo with no title text
+    "logo_only": True
+}
+html_logo = "img/docs_logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
